69cf4871ae7da8dd09368dde3c4ab680ec75e352
Ignacy Berent <2gb02ignac@gmail.com>
2024-10-19 17:46:44 +0200

    adding comments

--- urlshortener/frontend/views.py
+++ urlshortener/frontend/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
+    """
+    View function for the home page of the site. Its simple interface allows users to shorten URLs.
+    """
     return render(request, 'index.html')
 
 def list_urls(request):
+    """
+    View function for the page that lists all shortened URLs.
+    """
     return render(request, 'list_urls.html')
