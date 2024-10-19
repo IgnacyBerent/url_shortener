e6c122c10115c71d18ffe4fb70a5b79437e0b665
Ignacy Berent <2gb02ignac@gmail.com>
2024-10-19 17:43:54 +0200

    bugfixing tests

--- urlshortener/api/urls.py
+++ urlshortener/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import shorten_url, get_urls, delete_url, redirect_view, health_check, get_shorten_url
 
 urlpatterns = [
-    path("get_urls/", get_urls),
-    path("shorten_url/", shorten_url),
-    path("delete_url/<int:id>/", delete_url),
-    path("<str:short_url>/", redirect_view),
-    path("get_shorten_url/<int:id>/", get_shorten_url),
-    path("health_check/", health_check),
+    path("get_urls/", get_urls, name='get_urls'),
+    path("shorten_url/", shorten_url, name='shorten_url'),
+    path("delete_url/<int:id>/", delete_url, name='delete_url'),
+    path("<str:short_url>/", redirect_view, name='redirect_view'),
+    path("get_shorten_url/<int:id>/", get_shorten_url, name='get_shorten_url'),
+    path("health_check/", health_check, name='health_check'),
 ]
