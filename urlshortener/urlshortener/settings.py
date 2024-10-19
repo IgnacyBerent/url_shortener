dbb06755e78c966c87ca9c07eedebf6a330e9184
Ignacy Berent <2gb02ignac@gmail.com>
2024-10-19 15:21:07 +0200

    final celery fix

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -19,6 +19,14 @@ load_dotenv()
 # env variables
 SECRET_KEY = os.getenv("DJANGO_SECRET")
 
+# Celery settings
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
